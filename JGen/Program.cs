4825fb76137ccf07174a5d5f22b07e7fe11c12fc
Grigory Perepechko <grigoryp@playtika.by>
2014-07-13 18:42:15 +0300

    cleanup

--- JGen/Program.cs
+++ JGen/Program.cs
@@ -3,32 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using JGen.Generators;
-using JGen.MS;
 
 namespace JGen
 {
 	internal class Program
 	{
-		private static void Main(string[] args)
+		private static void Main()
 		{
-			//Perf.Foo();
-
 			Generator gen = CreateGeneratorForType(typeof (SF.Common.API.Model.GameNRE.NreSpinResponse));
 			ReaderCode reader = gen.GenerateReader();
 
-			Save(reader);
+			Save(reader, @"C:\Users\GrigoryP\Documents\visual studio 2012\Projects\JGen\JGen\Test\");
 
 			Console.ReadLine();
 		}
 
-		private static void Save(ReaderCode reader)
+		private static void Save(ReaderCode reader, string path)
 		{
-			File.WriteAllText(@"C:\Users\GrigoryP\Documents\visual studio 2012\Projects\JGen\JGen\Test\"+reader.ReaderName+".cs", reader.Content);
+			File.WriteAllText(Path.Combine(path, reader.ReaderName + ".cs"), reader.Content);
 			if (reader.Dependend != null)
 			{
 				foreach (var c in reader.Dependend)
 				{
-					Save(c);
+					Save(c, path);
 				}
 			}
 		}
@@ -60,47 +57,5 @@ namespace JGen
 	internal class TestType
 	{
 		public List<List<int>> data;
-	}
-
-	public class Address
-	{
-		[JSONValue(Name = "streetAddress")]
-		public string streetAddress { get; set; }
-		[JSONValue(Name = "city")]
-		public string city { get; set; }
-		[JSONValue(Name = "state")]
-		public string state { get; set; }
-		[JSONValue(Name = "postalCode")]
-		public string postalCode { get; set; }
-	}
-
-	public class PhoneNumber
-	{
-		public string type { get; set; }
-		public string number { get; set; }
-	}
-
-	public class Person
-	{
-		[JSONValue(Name = "firstName")]
-		public string firstName { get; set; }
-
-		[JSONValue(Name = "lastName")]
-		public string lastName { get; set; }
-
-		[JSONValue(Name = "isAlive")]
-		public bool isAlive { get; set; }
-
-		[JSONValue(Name = "age")]
-		public int age { get; set; }
-
-		[JSONValue(Name = "height_cm")]
-		public double height_cm { get; set; }
-
-		[JSONValue(Name = "address")]
-		public Address address { get; set; }
-
-		[JSONValue(Name = "phoneNumbers")]
-		public List<PhoneNumber> phoneNumbers { get; set; }
 	}
 }
